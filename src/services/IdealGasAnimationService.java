b98ee6ceb17f71e9a435fce82907d179ac1dfd6c
Summer Gautier <summergautier@gmail.com>
2020-05-09 14:10:16 -0700

    bounds detection added, need to attach circles as body of particle class. This is a crude start

--- src/services/IdealGasAnimationService.java
+++ src/services/IdealGasAnimationService.java
@@ -1,6 +1,8 @@
 package services;
 
 import javafx.animation.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -9,6 +11,7 @@ import models.Particle;
 import models.ParticleSystem;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ListIterator;
 import java.util.Random;
 
@@ -43,29 +46,62 @@ public class IdealGasAnimationService extends ParticleAnimationService {
         ArrayList<Circle> circlesList = new ArrayList<Circle>();
         Random random = new Random();
         int circleRadius = 5;
-
+        //init particles
         while(circlesList.size() <= 10) {
-            int startX = random.nextInt((int) animationPane.getWidth()) - circleRadius;
-            int startY = random.nextInt((int) animationPane.getHeight()) - circleRadius;
+            int startX = random.nextInt((int) animationPane.getWidth()) + circleRadius;
+            int startY = random.nextInt((int) animationPane.getHeight()) +  circleRadius;
             Circle circle = new Circle(startX, startY, circleRadius, Color.DARKRED);
+            HashMap<String, Integer> circleProperties = new HashMap<String, Integer>();
+            int speedInit = random.nextInt(5) + 1;
+            circleProperties.put("speedX", speedInit);
+            circleProperties.put("speedY", speedInit);
+            circle.getProperties().putAll(circleProperties);
             circle.setStroke(Color.RED);
             circlesList.add(circle);
         }
         animationPane.getChildren().addAll(circlesList);
 
-        ListIterator<Circle> circlesIterator = circlesList.listIterator();
-        while(circlesIterator.hasNext()) {
-            Circle circle = circlesIterator.next();
-            KeyValue kVMoveX = new KeyValue(circle.centerXProperty(), random.nextInt((int) animationPane.getWidth()) - circleRadius);
-            KeyValue kVRotate = new KeyValue(circle.rotateProperty(), random.nextInt((int) animationPane.getHeight()) - circleRadius);
-
-            KeyFrame keyFrame = new KeyFrame(Duration.millis(random.nextInt(2000) + 2000), kVMoveX, kVRotate);
-
-            timeline.getKeyFrames().add(keyFrame);
-        }
-
+        //set game loop to run continously (unless we call timeline.stop())
         timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.setAutoReverse(true);
+
+        //animation start time
+        final long timeStart = System.currentTimeMillis();
+
+        /*
+         * Particle Updates Calculated Every 60 seconds In This KeyFrame
+         */
+        KeyFrame kf = new KeyFrame(
+                //60 Frames Per Second
+                Duration.seconds(0.017),
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        //updating particle position
+                        ListIterator<Circle> circlesIterator = circlesList.listIterator();
+                        while(circlesIterator.hasNext()) {
+                            Circle circle = circlesIterator.next();
+
+                            //bounds detection
+                            if((circle.getCenterX() > animationPane.getWidth() - (circleRadius * 2)) || (circle.getCenterX() <= circleRadius)){
+                                circle.getProperties().replace("speedX", (int) circle.getProperties().get("speedX") * -1);
+                            }
+                            if((circle.getCenterY() > animationPane.getHeight() - (circleRadius * 2)) || (circle.getCenterY() <= circleRadius)){
+                                circle.getProperties().replace("speedY", (int) circle.getProperties().get("speedY") * -1);
+                            }
+
+                            int speedX = (int) circle.getProperties().get("speedX");
+                            int speedY = (int) circle.getProperties().get("speedY");
+
+                            circle.setCenterX(circle.getCenterX() + speedX);
+                            circle.setCenterY(circle.getCenterY() + speedY);
+                        }
+                    }
+                }
+        );
+
+        //add key frame to simulation loop
+        timeline.getKeyFrames().add(kf);
+        //begin simulation loop
         timeline.play();
 
     }
